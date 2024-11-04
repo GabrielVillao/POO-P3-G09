1140fa28bea33f082a3a0e67e8248d010e7467ca
GabrielVillao <gavillao@espol.edu.ec>
2024-11-03 23:13:29 -0500

    Confirmacion de commit

--- Aplicación/Perfil.java
+++ Aplicación/Perfil.java
@@ -2,6 +2,6 @@ package Aplicación;
 
 public class Perfil{
     public static void main(String[] args) {
-        System.out.println(null);
+        System.out.println("Este es mi nuevo commit");
     }
 }
