646d0ed74250e48ff958560c121922393f2ff301
GabrielVillao <gavillao@espol.edu.ec>
2024-11-04 22:21:22 -0500

    Commit clase Medicamento

--- Aplicación/Medicamentos.java
+++ Aplicación/Medicamentos.java
@@ -1,4 +1,19 @@
 package Aplicación;
-public class Medicamentos {
+
+import Enum.Presentación;
+
+public class Medicamentos extends Inventario {
+    private String frecuencia;
+    private String frecuenciaDia;
+    private String dosis;
+    public Medicamentos(String nombre, int cantidad, Presentación presentacion, String frecuencia, String frecuenciaDia,
+            String dosis) {
+        super(nombre, cantidad, presentacion);
+        this.frecuencia = frecuencia;
+        this.frecuenciaDia = frecuenciaDia;
+        this.dosis = dosis;
+    }
+
+    
     
 }
